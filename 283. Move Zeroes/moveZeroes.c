3a1c78ebad88dc9574f2202ae5641a65fe76940b
Mikhail <33933295+zhuzhman@users.noreply.github.com>
2021-09-16 16:05:41 +0300

    283 done

---
+++ 283. Move Zeroes/moveZeroes.c
@@ -0,0 +1,28 @@
+void	ft_int_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void moveZeroes(int* nums, int numsSize){
+    int head;
+    int tail;
+    int temp;
+    
+    head = 0;
+    tail = 0;
+    if ((numsSize == 1) || (numsSize == 0))
+        return ;
+    while (head < numsSize)
+    {
+        if (nums[head])
+        {
+            ft_int_swap(&nums[tail], &nums[head]);
+            tail++;
+        }
+        head++;
+    }
+}
